d627389cd3b7234a85ce29602d9b047b8e130437
Jacob Moss <jacobmoss09@gmail.com>
2019-10-02 13:10:51 -0500

    removed react

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
